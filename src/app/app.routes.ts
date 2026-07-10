eca336ed2a2535a611690ffac6628d583899d0aa
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-07-09 18:18:54 -0600

    fix: defining the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProyectPageComponent } from './pages/proyect-page/proyect-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    {
+        path: 'all-projects',
+        component: ProyectPageComponent
+    },
+    {
+        path: '**',
+        component: HomeComponent
+    },
+    //   { path: 'oldPath', redirectTo: '/staticPath' },  
+
+];
+
+  
