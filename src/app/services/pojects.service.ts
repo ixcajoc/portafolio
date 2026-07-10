7c930fa05aeb022b648b507432b214c35a0d7968
Denis Ixcajoc <denisixcajoc2001@gmail.com>
2026-07-09 18:34:18 -0600

    add: creating proyectService

---
+++ src/app/services/pojects.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Project } from '../Interfaces/proyect.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PojectsService {
+
+proyects: Project[] = [
+      {
+        name: 'Plataforma para entrega de diplomas digitales',
+        description: 'This is a description of Project 1.',
+        technologies: ['HTML', 'CSS', 'JavaScript'],
+        bg_proyect: 'images/cursos_municipales.jpg',
+        previewViewUrl:'https://ixcajoc.github.io/juventudcursos/',
+        frontendUrl:'https://github.com/ixcajoc/juventudcursos',
+        backendUrl:'',
+      },
+      {
+        name: 'Project 2',
+        description: 'This is a description of Project 2.',
+        technologies: ['SwiftUI', 'Combine'],
+        previewViewUrl:'https://example.com/project2',
+        frontendUrl:'https://example.com/project2/frontend',
+        backendUrl:'',
+      },
+      {
+        name: 'Project 3',
+        technologies: ['Next.js', 'Tailwind CSS'],
+        previewViewUrl:'https://example.com/project3',
+        frontendUrl:'https://example.com/project3/frontend',
+        backendUrl:'https://example.com/project3/backend',
+      },
+      {
+        name: 'Project 4',
+        technologies: ['Next.js', 'Tailwind CSS'],
+        previewViewUrl:'https://example.com/project4',
+        frontendUrl:'https://example.com/project4/frontend',
+        backendUrl:'https://example.com/project4/backend',
+      },
+   
+  
+    ];
+  
+}
